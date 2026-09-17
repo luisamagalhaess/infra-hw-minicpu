c14c5c08804e8534cb97fb74a058c48fd60c71b2
Luísa Magalhães <lfm3@cesar.school>
2026-09-17 12:19:25 -0300

    implementei decodificacao de instrucoes

--- main.py
+++ main.py
@@ -13,3 +13,39 @@ class MiniCPU:
 
         self.rodando = True
         self.ciclo = 0
+        
+    def decode(self, opcode, operando1, operando2):
+        if opcode == 0x01:
+            instrucao = "LOAD"
+
+        elif opcode == 0x02:
+            instrucao = "STORE"
+
+        elif opcode == 0x03:
+            instrucao = "ADD"
+
+        elif opcode == 0x04:
+            instrucao = "SUB"
+
+        elif opcode == 0x05:
+            instrucao = "MOV"
+
+        elif opcode == 0x06:
+            instrucao = "CMP"
+
+        elif opcode == 0x07:
+            instrucao = "JMP"
+
+        elif opcode == 0x08:
+            instrucao = "JZ"
+
+        elif opcode == 0x09:
+            instrucao = "JNZ"
+
+        elif opcode == 0x0A:
+            instrucao = "HALT"
+
+        else:
+            instrucao = "INVALIDA"
+
+        return instrucao, operando1, operando2
